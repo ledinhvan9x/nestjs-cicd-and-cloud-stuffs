321e09e9422dd7e937ba5702a4bd22cbf2cb8a59
dinhvan <dinhvanle9xpro@gmail.com>
2026-04-26 11:10:24 +0700

    add cache full

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World test update not allowed!';
+    return 'Hello World!';
   }
 }
