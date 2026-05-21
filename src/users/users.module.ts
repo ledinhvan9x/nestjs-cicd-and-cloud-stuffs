722551f6d220b062e9707b585b34d204e27e097d
dinhvan <dinhvanle9xpro@gmail.com>
2026-05-21 20:19:33 +0700

    small changes

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,7 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RedisService } from 'src/redis/redis.service';
+import { RedisService } from '@app/redis/redis.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
