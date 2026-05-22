2a342aaa4792525d5a41442b17fb877f853ca77c
dinhvan <dinhvanle9xpro@gmail.com>
2026-05-22 13:31:10 +0700

    edit typorm

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -13,4 +13,8 @@ export default new DataSource({
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
+  ssl:
+    process.env.NODE_ENV !== 'development'
+      ? { rejectUnauthorized: false }
+      : false,
 });
