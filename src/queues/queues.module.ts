85ec6e2ec40c4f376141de945f38abbfc0a40b72
ledinhvan9x <99377269+ledinhvan9x@users.noreply.github.com>
2026-08-30 21:35:40 +0700

    Update queues.module.ts

--- src/queues/queues.module.ts
+++ src/queues/queues.module.ts
@@ -15,6 +15,8 @@ import { TestQueueService } from './test.service';
         backoff: {
           type: 'exponential',
           delay: 1000,
+        removeOnFail: 1000, // default false (mean keep all msgs) => keep 1000 latest
+        removeOnComplete: 1000, // default false (mean keep all msgs) => keep 1000 latest
     },
   },
     }),
